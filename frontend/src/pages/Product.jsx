e2b4e301a97a1c361a306ac8661a8e3d36ea2c9d
seddikh49 <seddikh49@gmail.com>
2025-04-24 14:42:52 +0100

    from

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -15,16 +15,15 @@ const Product = () => {
   
   
 
-  const [image, setImage] = useState();
+  const [imageIndex, setimageIndex] = useState(0);
   const { productId } = useParams();
   const [product, setproduct] = useState(null);
   
 
-  
   const getSingleProduct = async() => {
     const singleProduct = products.find((product) => product._id === productId);
     setproduct(singleProduct);
-    console.log(product)
+
   
   }
   
@@ -36,24 +35,42 @@ const Product = () => {
 
 
 
-  return  (
-    <div className='w-full max-h-max  flex xl:flex-row lg:flex-row md:flex-col sm:flex-col xm:flex-col items-center '>
-      {/* <div className='xl:w-1/2 lg:w-1/2 md:w-full flex flex-col gap-2 justify-center  items-center'>
-           <img src={product.image[0]} alt="" className='w-2/3' />  
-           <div className='grid grid-cols-4 w-2/3 gap-2 '>
+  return product &&  (
+    <div className='w-full max-h-max  flex xl:flex-row lg:flex-row md:flex-col sm:flex-col xm:flex-col '>
+ 
+      
+      <div className='xl:w-1/2 lg:w-1/2 md:w-full  h-max  flex flex-col gap-10 items-end pr-10  '>
+         <div className='flex flex-col items-end'> 
+          <p>{product.name}</p>
+          <h1>{product.price}</h1>
+         </div>
+         <form className='flex flex-col w-[600px] gap-5 items-end' action="">
+          <div className='w-[600px] flex gap-5 '>
+            <input className='w-1/2 bg-gray-100 py-3 font-bold px-2 border-1 border-blue-500 rounded-[5px]' placeholder='الاسم الكامل' type="text" />
+            <input className='w-1/2 bg-gray-100 py-3 font-bold px-2 border-1 rounded-[5px]' type="number" id="age" name="age" min="1" max="100" placeholder=" رقم الهاتف" />
+          </div>
+          <div className='w-[600px] flex gap-5'>
+            <select className='w-1/2 bg-gray-100 py-3 font-bold px-2 border-1 rounded-[5px]' name="" id="">
+               <option value="">الولاية</option>
+            </select>
+            <select className='w-1/2 bg-gray-100 py-3 font-bold px-2 border-1 rounded-[5px]' name="" id="">
+               <option value="">البلدية</option>
+            </select>  
+          </div>
+         </form>
+      </div>
+
+
+      <div className='xl:w-1/2 lg:w-1/2 md:w-full flex flex-col gap-2 justify-center  items-start'>
+           <img src={product.image[imageIndex]} alt="" className='w-3/5' />  
+           <div className='grid grid-cols-4 w-3/5 gap-2 '>
             {product.image.map((img,index)=>{
               return(
-                <img key={index}  src={img} alt="" />
+                <img key={index} onClick={()=> setimageIndex(index)} className='cursor-pointer'  src={img} alt="" />
               )
             })}
             </div> 
-      </div> */}
-      {product && (
-        <h1> {product.name}</h1>
-      )}
-     
-      <div className='xl:w-1/2 lg:w-1/2 md:w-full border-1 h-max '></div>
-  
+      </div>
     </div>
 
   )
