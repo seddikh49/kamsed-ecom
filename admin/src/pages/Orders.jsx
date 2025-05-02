c44cc06f648f31fa6b7dbd9c42737cb69aec7d6b
seddikh49 <seddikh49@gmail.com>
2025-05-02 11:05:08 +0100

    listorder

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -4,6 +4,9 @@ import { backEndUrl } from '../App'
 import { NavLink } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import { toast } from 'react-toastify';
+import { FaEye } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+
 
 
 
@@ -44,13 +47,13 @@ const Orders = () => {
       icon: 'warning',
       confirmButtonText: 'نعم',
       showCancelButton: true,
-      cancelButtonText: 'إلغاء', // ← هنا تغيّر النص
+      cancelButtonText: 'إلغاء',
 
     }).then(async (result) => {
       if (result.isConfirmed) {
         try {
           const response = await axios.delete(`${backEndUrl}/api/order/delete/${id}`)
-          if(response){
+          if (response) {
             toast.success(response.data.msg)
             fetchOrders()
           }
@@ -67,34 +70,65 @@ const Orders = () => {
 
 
   return (
-    <div className='w-full'>
-      <div className='flex gap-4  justify-center items-center  w-full  '>
-        <table className="w-full  text-center border border-gray-300">
-          <thead className="bg-gray-200">
-            <tr>
-              <th className="py-2 border w-[200px] "></th>
-              <th className="py-2 border w-[200px] ">رقم الهاتف</th>
-              <th className=" py-2 border w-[200px]">الاسم الكامل</th>
-              <th className=" py-2 border  w-[200px]"> العدد</th>
-            </tr>
-          </thead>
-          <tbody>
-            {orders.map((order, index) => {
-              return (
-                <tr key={index} className="border border-gray-300">
-                  <td className='py-2 border flex gap-10 justify-center'>
-                    <button onClick={()=>handleDelete(order._id)} className='cursor-pointer bg-red-500 text-white py-2 px-3 font-bold rounded-md'>حذف الطلب </button>
-                    <NavLink to={`/orderdetail/${order.fullName}`} className='cursor-pointer bg-green-500 text-white py-2 px-3 font-bold rounded-md'>تفاصيل الطلب</NavLink>
-                  </td>
-                  <td className="py-1 border">{order.phone} </td>
-                  <td className="py-1 border"> {order.fullName}</td>
-                  <td className="py-1 border">{index}</td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
+    <div className='w-full bg-gray-100 p-10'>
+      <div className='p-5 bg-white rounded-xl shadow-xl'>
+        <div className='flex'>
+          <select class="border p-2 rounded">
+            <option>Status</option>
+            <option>New</option>
+            <option>Processing</option>
+            <option>Delivered</option>
+            <option>Cancelled</option>
+          </select>
+          <select class="border p-2 rounded">
+            <option>Date</option>
+            <option>Today</option>
+            <option>This week</option>
+          </select>
+          <div>
+            <input type="text" placeholder='البحث في الطلبات' />
+          </div>
+        </div>
+        <div className='flex gap-4 bg-amber-50  justify-center items-center  w-full overflow-hidden rounded-xl border border-gray-300  '>
+          <table className="w-full  text-center bg-white  border-gray-300">
+            <thead className="bg-gray-100 rounded-md">
+              <tr>
+                <th className="py-2 border-gray-200 border w-[50px] ">الإجراء</th>
+                <th className="py-2 border-gray-200 border w-[50px] ">الحالة</th>
+                <th className="py-2 border border-gray-200 w-[100px] ">تاريخ الطلب</th>
+                <th className="py-2 border border-gray-200 w-[100px] ">رقم الهاتف</th>
+                <th className=" py-2 border border-gray-200 w-[100px]">الاسم الكامل</th>
+                <th className=" py-2 border  border-gray-200  w-[50px]"> الطلب</th>
+              </tr>
+            </thead>
+            <tbody>
+              {orders.map((order, index) => {
+                return (
+                  <tr key={index} className="border border-gray-300">
+                    <td className='py-2  flex border-gray-200 gap-5 justify-center'>
+                      <button onClick={() => handleDelete(order._id)} className='cursor-pointer bg-red-500 text-white py-2 px-3 font-bold rounded-md'>
+                        <MdDelete />
+                      </button>
+
+                      <NavLink to={`/orderdetail/${order.fullName}`} className='cursor-pointer bg-green-500 text-white py-2 px-3 font-bold rounded-md'>
+                        <FaEye />
+
+                      </NavLink>
+                    </td>
+                    <td></td>
+                    <td className="py-1 border-b-gray-200 ">{order.date} </td>
+                    <td className="py-1 border-b-gray-200 ">{order.phone} </td>
+                    <td className="py-1 border-b-gray-200 "> {order.fullName}</td>
+                    <td className="py-1 border border-gray-200  ">{index} #</td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
+
       </div>
+
     </div>
   )
 }
